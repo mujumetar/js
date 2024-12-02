efd991dd5c60b19b87ea23de52e6fa7a63217633
WebersIO <muzammilmetar82@gmail.com>
2024-12-02 17:48:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// let a = 10
+// console.log(typeof a);
+
+// let b = String(a)
+// console.log(typeof b);
+
+
+// console.log(a , b)
+
+// let obj = {
+//     name: "mujju",
+//     age: 20,
+//     course : "full-stack web developer",
+//     city: "bhavnagar",
+//     portfolio: "https://muzammil-two.vercel.app/",
+// };
+
+// console.log(obj);
+
+
+let arr = [4,2,5,6,7,8,9,3,1,0]
+
+// arr.sort(); // sorted
+// arr.push(9) - // added last value 
+// arr.join("-") // X
+// arr.pop() // removes the last
+// arr.splice(5,5) // from this index to count and printed
+// arr.reverse() // reversed
+
+
+console.log(arr);
